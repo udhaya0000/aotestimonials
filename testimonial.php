33b3d4395182f92f2ef84790d91b087ff9c26b9f
udhay <udhaya0000@gmail.com>
2019-06-25 17:06:35 +0530

    Fixes

--- testimonial.php
+++ testimonial.php
@@ -20,6 +20,9 @@ class Testimonials {
 
    // Represents the nonce value used to save the post media
 	 private $nonce = 'wp_testimonials_nonce';
+	 private $singular_label = "Testimonial";
+	 private $plural_label = "Testimonials";
+
 
 	/* --------------------------------------------
 	 * Constructor
@@ -36,9 +39,10 @@ class Testimonials {
 
 	 	// Setup the meta box hooks
     add_action( 'init', array($this, 'create_cpt') );
-    add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ) );
     add_action( 'save_post', array( $this, 'save_testimonials_meta' ) );
 		add_action( 'admin_enqueue_scripts', array($this, 'load_wp_media_files') );
+
+		$this->add_meta_box();
 	 } // end construct
 
 	 function load_wp_media_files() {
@@ -74,22 +78,19 @@ class Testimonials {
 	public function create_cpt() {
 
     $theme = "estpal";
-    $singlur_label = "Testimonial";
-    $plural_label = "Testimonials";
-
     // Set UI labels for Custom Post Type
     $labels = array(
-            'name'                => _x($plural_label, 'Post Type General Name', $theme),
-            'singular_name'       => _x($singlur_label, 'Post Type Singular Name', $theme),
-            'menu_name'           => __($plural_label, $theme),
-            'parent_item_colon'   => __('Parent '.$singlur_label, $theme),
-            'all_items'           => __('All '.$plural_label, $theme),
-            'view_item'           => __('View '.$singlur_label, $theme),
-            'add_new_item'        => __('Add New '.$singlur_label, $theme),
+            'name'                => _x($this->plural_label, 'Post Type General Name', $theme),
+            'singular_name'       => _x($this->singular_label, 'Post Type Singular Name', $theme),
+            'menu_name'           => __($this->plural_label, $theme),
+            'parent_item_colon'   => __('Parent '.$this->singular_label, $theme),
+            'all_items'           => __('All '.$this->plural_label, $theme),
+            'view_item'           => __('View '.$this->singular_label, $theme),
+            'add_new_item'        => __('Add New '.$this->singular_label, $theme),
             'add_new'             => __('Add New', $theme),
-            'edit_item'           => __('Edit '.$singlur_label, $theme),
-            'update_item'         => __('Update '.$singlur_label, $theme),
-            'search_items'        => __('Search '.$singlur_label, $theme),
+            'edit_item'           => __('Edit '.$this->singular_label, $theme),
+            'update_item'         => __('Update '.$this->singular_label, $theme),
+            'search_items'        => __('Search '.$this->singular_label, $theme),
             'not_found'           => __('Not Found', $theme),
             'not_found_in_trash'  => __('Not found in Trash', $theme),
         );
@@ -98,7 +99,7 @@ class Testimonials {
 
     $args = array(
             'label'               => __('testimonials', $theme),
-            'description'         => __('List of '.$plural_label, $theme),
+            'description'         => __('List of '.$this->plural_label, $theme),
             'labels'              => $labels,
             // Testimonials this CPT supports in Post Editor
             'supports'            => array('title', 'revisions'),
@@ -126,85 +127,47 @@ class Testimonials {
 
 	} // add_file_meta_box
 
-  public function add_meta_box() {
-    add_meta_box(
-      "testimonial-meta-box",
-      "Testimonial Info",
-      array($this, "display_testimonials_meta"),
-      "testimonials",
-      "advanced",
-      "high",
-      null);
-  }
+	public function add_meta_box()
+	{
+			add_action('cmb2_admin_init', array($this, 'register_metabox'));
+	}
 
-  public function save_testimonials_meta($post_id) {
-    // First, make sure the user can save the post
-		if( $this->user_can_save( $post_id, $this->nonce ) ) {
-
-      $this->save_testimonials($post_id);
-		} // end if
-
-  }
-
-  public function display_testimonials_meta() {
-    global $post;
-    wp_nonce_field( plugin_basename( __FILE__ ), $this->nonce );
-
-    $values = get_post_custom($post->ID);
-    $testimonial = isset($values['testimonial']) ? json_decode($values['testimonial'][0]) : null;
-    //var_dump($testimonial);
-    ?>
-		<div class="form-group">
-      <div class="font-weight-bold mb-1">Author Title</div>
-      <input type="text" class="form-control" name="testimonial[auther_title]" value="<?php echo (isset($testimonial) && isset($testimonial->auther_title)) ? $testimonial->auther_title : ""; ?>">
-    </div>
-		<div class="form-group">
-      <div class="font-weight-bold mb-1">Author Company</div>
-      <input type="text" class="form-control" name="testimonial[auther_co]" value="<?php echo (isset($testimonial) && isset($testimonial->auther_co)) ? $testimonial->auther_co : ""; ?>">
-    </div>
-    <div class="js-form-group form-group">
-      <div class="font-weight-bold mb-1">Testimonial Icon</div>
-      <div class="js-img-wrap mb-2">
-        <?php if(isset($testimonial)): ?>
-          <img src="<?php echo $testimonial->icon_url; ?>" alt=""/>
-        <?php endif; ?>
-      </div>
-      <div class="input-group">
-        <input id="testimonial_icon_url" type="text" class="form-control" name="testimonial[icon_url]" value="<?php echo isset($testimonial) ? $testimonial->icon_url : ""; ?>">
-        <div class="input-group-append">
-          <button type="button" data-media-uploader-target="#testimonial_icon_url" class="btn btn-secondary js-add-icon">Add Icon</button>
-        </div>
-      </div>
-    </div>
-    <div class="form-group">
-      <div class="font-weight-bold">Testimonial Content</div>
-      <textarea name="testimonial[text]" class="form-control" ><?php echo isset($testimonial) ? $testimonial->text : ""; ?></textarea>
-    </div>
-
-    <?php
-  }
-
-  public function save_testimonials($post_id) {
-    if(isset($_POST['testimonial'])) {
-      update_post_meta($post_id, "testimonial", json_encode($_POST['testimonial']));
-    }
-  }
-  /**
-	 * Determines whether or not the current user has the ability to save meta data associated with this post.
-	 *
-	 * @param		int		$post_id	The ID of the post being save
-	 * @param		bool				Whether or not the user has the ability to save this post.
+	/**
+	 * Hook in and add a metabox that only appears on the 'About' page
 	 */
-	function user_can_save( $post_id, $nonce ) {
+	public function register_metabox()
+	{
+			$prefix = strtolower($this->singular_label).'_';
 
-	    $is_autosave = wp_is_post_autosave( $post_id );
-	    $is_revision = wp_is_post_revision( $post_id );
-	    $is_valid_nonce = ( isset( $_POST[ $nonce ] ) && wp_verify_nonce( $_POST[ $nonce ], plugin_basename( __FILE__ ) ) );
+			$cmb_page = new_cmb2_box([
+			 'id'           => $prefix . 'metabox',
+			 'title'        => esc_html__($this->singular_label.' Info', 'cmb2'),
+			 'object_types' => array( strtolower($this->plural_label) ), // Post type
+			 'context'      => 'normal',
+			 'priority'     => 'default',
+			 'show_names'   => true, // Show field names on the left
+			]);
 
-	    // Return true if the user is able to save; otherwise, false.
-	    return ! ( $is_autosave || $is_revision ) && $is_valid_nonce;
 
-	} // end user_can_save
+		$cmb_page->add_field( array(
+			'name' => 'Author Location',
+			'desc' => '',
+			'id'   => $prefix.'location',
+			'type' => 'text',
+		) );
+
+		$cmb_page->add_field( array(
+			'name' => 'Author Feedback',
+			'id'   => $prefix.'feedback',
+			'type' => 'text',
+		) );
+
+		$cmb_page->add_field( array(
+			'name' => 'Author Image',
+			'id'   => $prefix.'img',
+			'type' => 'file',
+		) );
+	}
 } // end class
 
-$GLOBALS['testimonial'] = new Testimonials();
+$GLOBALS['testimonials'] = new Testimonials();
